0682e5a75ddb2582860f274882d78d33d0dc58b0
techinezz <y.martinez.tech@gmail.com>
2025-10-17 14:10:44 -0400

    update chat

--- my-app/app/AIChat.tsx
+++ my-app/app/AIChat.tsx
@@ -83,7 +83,7 @@ export default function AIChat() {
             <div
               className={`max-w-[80%] rounded-lg px-4 py-2 ${
                 message.role === 'user'
-                  ? 'bg-blue-500 text-white'
+                  ? 'bg-[#ABD2A9] text-gray-900'
                   : 'bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100'
               }`}
             >
